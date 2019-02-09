f08456a941b48b088e27345325381e74e966a137
will7hughes <will7hughes@gmail.com>
2019-02-09 13:33:58 -0600

    Installed react-navigation, setup basic screen

--- android/app/src/main/java/com/slowdown/MainActivity.java
+++ android/app/src/main/java/com/slowdown/MainActivity.java
@@ -2,7 +2,12 @@ package com.slowdown;
 
 import com.facebook.react.ReactActivity;
 
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+
 public class MainActivity extends ReactActivity {
+
 
     /**
      * Returns the name of the main component registered from JavaScript.
@@ -11,5 +16,16 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "slowdown";
+    }
+
+
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+      return new ReactActivityDelegate(this, getMainComponentName()) {
+        @Override
+        protected ReactRootView createRootView() {
+         return new RNGestureHandlerEnabledRootView(MainActivity.this);
+        }
+     };
     }
 }
